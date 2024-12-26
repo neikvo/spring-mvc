4f5759bf0370922f0f5e93c2ffdecd0b36805f19
neikvo <kien.vo@cnv.vn>
2024-12-26 13:41:43 +0700

    video 48

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,38 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.hoidanit.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        return "hello";
+    }
+}
+
+//@RestController
+//public class UserController {
+//
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/")
+//    public String getHomePage() {
+//        return this.userService.handleHello();
+//    }
+//}
